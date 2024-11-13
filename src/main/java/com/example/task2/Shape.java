58e7753fd692039046df0d94c066a6c42e36df7d
AresFighter <artemiy.u.97@mail.ru>
2024-11-14 01:46:16 +0300

    Добавление в Shape полей x, y, а также Object clone()

--- src/main/java/com/example/task2/Shape.java
+++ src/main/java/com/example/task2/Shape.java
@@ -4,11 +4,28 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
 public abstract class Shape {
+
+    protected double x;
+
+    protected double y;
+
     protected Color color; // Общий цвет для всех фигур
 
-    public Shape(Color color) {
+    public Shape(double x, double y, Color color) {
+        this.x = x;
+        this.y = y;
         this.color = color;
     }
     public abstract void draw(GraphicsContext gr);
     public abstract String descriptor();
+
+    @Override
+    public Object clone() {
+        try {
+            return super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
