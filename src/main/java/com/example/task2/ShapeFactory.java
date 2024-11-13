c019cb9bd6d9ba1aa8767889690aafcdb0d2702e
AresFighter <artemiy.u.97@mail.ru>
2024-11-14 02:08:52 +0300

    Изменение параметров фигур по умолчанию, изменение считывания фигур

--- src/main/java/com/example/task2/ShapeFactory.java
+++ src/main/java/com/example/task2/ShapeFactory.java
@@ -3,20 +3,17 @@ package com.example.task2;
 import javafx.scene.paint.Color;
 
 public class ShapeFactory {
-    public Shape createShape(int numberOfSides) {
-        switch (numberOfSides) {
-            case 0:
-                return new Circle(Color.CORAL);
-            case 1:
-                return new Straight(Color.DARKBLUE);
-            case 2:
-                return new Angle(Color.BLACK);
-            case 3:
-                return new Triangle(Color.INDIGO);
-            case 4:
-                return new Square(Color.DARKORANGE);
-            case 5:
-                return new Pentagon(Color.DARKOLIVEGREEN);
+    public Shape createShape(String shapeType, double x, double y, Color color) {
+        switch (shapeType.toLowerCase()) {
+            case "круг":
+                //параметры по умолчанию
+                return new Circle(x, y, 10, color);
+            case "прямоугольник":
+                return new Rectangle(x, y, 10, 5, color);
+            case "треугольник":
+                return new Triangle(x, y, 15, 5, 13, color);
+            case "плюс":
+                return new Plus(x, y, 10, color); // Пример дельты
             default:
                 return null;
         }
