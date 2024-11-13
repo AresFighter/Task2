78aa533af38a6c711a388c2bd0f30d4be028fda5
AresFighter <artemiy.u.97@mail.ru>
2024-11-14 02:00:07 +0300

    Добавление полей alpha, beta, omega, изменение реализации построения фигуры

--- src/main/java/com/example/task2/Triangle.java
+++ src/main/java/com/example/task2/Triangle.java
@@ -5,16 +5,24 @@ import javafx.scene.paint.Color;
 
 public class Triangle extends Shape {
 
-    public Triangle(Color color) {
-        super(color);
+    private final double alpha; // Смещение вершины вправо
+    private final double beta;  // Смещение вершины влево
+    private final double omega; // Высота треугольника
+
+    public Triangle(double x, double y, double alpha, double beta, double omega, Color color) {
+        super(x, y, color);
+        this.alpha = alpha;
+        this.beta = beta;
+        this.omega = omega;
     }
 
     @Override
     public void draw(GraphicsContext gr) {
         gr.setFill(color);
 
-        double[] xPoints = {200, 250, 150};
-        double[] yPoints = {100, 200, 200};
+        // Определяем координаты трёх точек
+        double[] xPoints = {x, x + alpha, x - beta};
+        double[] yPoints = {y, y + omega, y + omega};
 
         gr.fillPolygon(xPoints, yPoints, 3);
     }
