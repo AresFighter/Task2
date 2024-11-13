5577f353411be45674f4dc94c8869423f02220e9
AresFighter <artemiy.u.97@mail.ru>
2024-11-14 00:27:08 +0300

    Добавлены цвета фигур

--- src/main/java/com/example/task2/ShapeFactory.java
+++ src/main/java/com/example/task2/ShapeFactory.java
@@ -1,20 +1,22 @@
 package com.example.task2;
 
+import javafx.scene.paint.Color;
+
 public class ShapeFactory {
     public Shape createShape(int numberOfSides) {
         switch (numberOfSides) {
             case 0:
-                return new Circle();
+                return new Circle(Color.CORAL);
             case 1:
-                return new Straight();
+                return new Straight(Color.DARKBLUE);
             case 2:
-                return new Angle();
+                return new Angle(Color.BLACK);
             case 3:
-                return new Triangle();
+                return new Triangle(Color.INDIGO);
             case 4:
-                return new Square();
+                return new Square(Color.DARKORANGE);
             case 5:
-                return new Pentagon();
+                return new Pentagon(Color.DARKOLIVEGREEN);
             default:
                 return null;
         }
